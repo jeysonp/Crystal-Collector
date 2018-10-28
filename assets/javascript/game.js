9241dc512bd3f7efea030ce0a6c66d9ec049d807
jeybone <jeysonp@gmail.com>
2018-10-27 23:38:32 -0500

    added random numbers, rules area, and minimal styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,27 @@
+
 $(document).ready(function() {
 
-  var targetNumber = 55;
+  var wins = 0;
+  var losses = 0;
+  var targetNumber;
+
+  targetNumber = Math.floor(Math.random() * 102) + 19;
 
   $("#number-to-guess").text(targetNumber);
 
   var counter = 0;
-  var numberOptions = [10, 5, 3, 7];
+  var numberOptions = "";
+  
 
-  var increment = numberOptions[Math.floor(Math.random())];
-  for (var i = 0; i < numberOptions.length; i++) {
+  for (var i = 0; i < 4; i++) {
+    var numberOptions = Math.floor(Math.random() * 12 + 1);
+    console.log(numberOptions);
+
     var imageGem = $("<img>");
     imageGem.addClass("gem-image");
     imageGem.attr("src", "assets/images/red-gem.jpg")
-    imageGem.attr("data-gemvalue", numberOptions[i]);
+    imageGem.attr("src", "assets/images/green-gem.jpg")
+    imageGem.attr("data-gemvalue", numberOptions);
     $("#gems").append(imageGem);
   }
 
@@ -20,33 +29,18 @@ $(document).ready(function() {
     var gemValue = ($(this).attr("data-gemvalue"));
     gemValue = parseInt(gemValue);
       counter += gemValue;
-      alert("New score: " + counter);
+      console.log("New score: " + counter);
       
   if (counter === targetNumber) {
-      alert("You win!");
+      $("#wins").html(wins);
+      console.log("You win!");
   } 
 
   else if (counter >= targetNumber) {
-      alert("You Lose!");
-
+    $("#losses").html(losses);
+      console.log("You Lose!");
   }
-
+ 
   });
 
 });
-
-
-
-
-
-// for(var i = 0; i <4; i++) {
-
-//     var randomNumber = Math.floor(Math.random() * 11 + 1);
-//     console.log(randomNumber);
-
-//     var gem = $("<div>");
-//         gem.attr("class", "gem", "data-gem");
-
-//     $(".gems").append(gem);
-
-// }
